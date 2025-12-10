d8caa8b2edfd7f8db8128ed529a7a3dd99f90888
macrobian88 <ramshankarsc9@gmail.com>
2025-12-10 12:26:24 +0530

    Add setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open("requirements.txt") as f:
 setup(
     name="fcrm_custom_app",
     version="0.0.1",
-    description="Custom Frappe CRM App with Campaign Management",
-    author="FCRM Developer",
-    author_email="developer@example.com",
+    description="Custom Frappe CRM App with Customer Feedback DocType",
+    author="Your Name",
+    author_email="your@email.com",
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
